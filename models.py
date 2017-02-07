53da14ff119ea660b06b8ddf695b0838d848ed0f
James <jjlong@mit.edu>
2017-02-07 18:01:03 -0500

    updating schema

--- models.py
+++ models.py
@@ -2,18 +2,40 @@ from app import db
 from sqlalchemy.dialects.postgresql import JSON
 
 
-class Result(db.Model):
-    __tablename__ = 'results'
+class Computation(db.Model):
+    __tablename__ = 'computation'
 
     id = db.Column(db.Integer, primary_key=True)
-    url = db.Column(db.String())
-    result_all = db.Column(JSON)
-    result_no_stop_words = db.Column(JSON)
+    node_id = db.Column(db.Integer)
+    server_time = db.Column(db.Float)
+    node_time = db.Column(db.Float)
+    date = db.Column(db.DateTime)
 
-    def __init__(self, url, result_all, result_no_stop_words):
-        self.url = url
-        self.result_all = result_all
-        self.result_no_stop_words = result_no_stop_words
+    def __init__(self, server_time, node_time, date):#
+        self.node_id = node_id
+        self.server_time = server_time
+        self.node_time = node_time
+        self.date = date
+
+    def __repr__(self):
+        return '<id {}>'.format(self.id)
+
+class Communication(db.Model):
+    __tablename__ = 'communication'
+
+    id = db.Column(db.Integer, primary_key=True)
+    node_id = db.Column(db.Integer)
+    to_node_id = db.Column(db.Integer)
+    data_length = db.Column(db.Integer)
+    comm_time = db.Column(db.Float)
+    date = db.Column(db.DateTime)
+
+    def __init__(self, server_time, node_time, date):#
+        self.node_id = node_id
+        self.to_node_id = to_node_id
+        self.data_length = data_length
+        self.comm_time = comm_time
+        self.date = date
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
