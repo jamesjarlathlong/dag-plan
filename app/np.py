2653d8ca6dbefb2fef9da52788f1810f0df13c30
james <jjlong@mit.edu>
2017-07-30 15:15:08 -0400

    recursive call to wrong fnc name fixed

--- app/np.py
+++ app/np.py
@@ -156,8 +156,8 @@ def get_column_as_vec(nested_lst, idx):
 def radix2(x):
     N = len(x)
     if N <= 1: return x
-    even = fft(x[0::2])
-    odd =  fft(x[1::2])
+    even = radix2(x[0::2])
+    odd =  radix2(x[1::2])
     T= [cmath.exp(-2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
     return [even[k] + T[k] for k in range(N//2)] + \
            [even[k] - T[k] for k in range(N//2)]
